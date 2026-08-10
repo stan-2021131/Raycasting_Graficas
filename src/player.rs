ac9916116d8d8cc9081a6c69b7af25a92d30d395
stan-2021131 <sergiocoromac@gmail.com>
2026-08-09 19:03:15 -0600

    feat: Agregar detección de colisiones y capacidad de deslizamiento al jugador

--- src/player.rs
+++ src/player.rs
@@ -1,14 +1,25 @@
 use minifb::{Key, Window};
 use nalgebra_glm::Vec2;
 use std::f32::consts::PI;
+use crate::maze::{is_wall, Maze};
 
 pub struct Player {
     pub pos: Vec2,
     pub a: f32,
 }
 
-pub fn process_events(window: &Window, player: &mut Player) {
+/*
+* Procesa los eventos del teclado y mueve al jugador
+* Parametros:
+* window: ventana del juego
+* player: jugador
+* maze: laberinto
+* block_size: tamaño de cada celda del laberinto
+*/
+pub fn process_events(window: &Window, player: &mut Player, maze: &Maze, block_size: usize) {
+    //velocidad de movimiento
     const MOVE_SPEED: f32 = 10.0;
+    //velocidad de rotacion
     const ROTATION_SPEED: f32 = PI / 10.0;
 
     if window.is_key_down(Key::A) {
@@ -20,12 +31,28 @@ pub fn process_events(window: &Window, player: &mut Player) {
     }
 
     if window.is_key_down(Key::W) {
-        player.pos.x += MOVE_SPEED * player.a.cos();
-        player.pos.y += MOVE_SPEED * player.a.sin();
+        let next_x = player.pos.x + MOVE_SPEED * player.a.cos();
+        let next_y = player.pos.y + MOVE_SPEED * player.a.sin();
+
+        if !is_wall(maze, next_x, player.pos.y, block_size) {
+            player.pos.x = next_x;
+        }
+
+        if !is_wall(maze, player.pos.x, next_y, block_size) {
+            player.pos.y = next_y;
+        }
     }
 
     if window.is_key_down(Key::S) {
-        player.pos.x -= MOVE_SPEED * player.a.cos();
-        player.pos.y -= MOVE_SPEED * player.a.sin();
+        let next_x = player.pos.x - MOVE_SPEED * player.a.cos();
+        let next_y = player.pos.y - MOVE_SPEED * player.a.sin();
+
+        if !is_wall(maze, next_x, player.pos.y, block_size) {
+            player.pos.x = next_x;
+        }
+
+        if !is_wall(maze, player.pos.x, next_y, block_size) {
+            player.pos.y = next_y;
+        }
     }
 }
