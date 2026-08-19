3881c9f3b9d8daca506d94bcd0d12b5b84e989de
stan-2021131 <sergiocoromac@gmail.com>
2026-08-19 17:48:32 -0600

    feat: Rotación con mouse implementada

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-use minifb::{Key, Window};
+use minifb::{Key, MouseMode, Window};
 use nalgebra_glm::Vec2;
 use std::f32::consts::PI;
 use crate::maze::{is_wall, Maze};
@@ -16,11 +16,13 @@ pub struct Player {
 * maze: laberinto
 * block_size: tamaño de cada celda del laberinto
 */
-pub fn process_events(window: &Window, player: &mut Player, maze: &Maze, block_size: usize) {
+pub fn process_events(window: &mut Window, player: &mut Player, maze: &Maze, block_size: usize, last_mouse_x: &mut Option<f32>) {
     //velocidad de movimiento
     const MOVE_SPEED: f32 = 10.0;
     //velocidad de rotacion
     const ROTATION_SPEED: f32 = PI / 10.0;
+    //sensibilidad del mouse
+    const MOUSE_SENSITIVITY: f32 = 0.005;
 
     if window.is_key_down(Key::A) {
         player.a -= ROTATION_SPEED;
@@ -41,6 +43,24 @@ pub fn process_events(window: &Window, player: &mut Player, maze: &Maze, block_s
         if !is_wall(maze, player.pos.x, next_y, block_size) {
             player.pos.y = next_y;
         }
+    }
+
+    // Movimiento relativo del mouse
+    if let Some((mouse_x, _mouse_y)) =
+        window.get_mouse_pos(MouseMode::Pass)
+    {
+        // Primera lectura:
+        // solamente guardamos la posición.
+        if let Some(previous_x) = *last_mouse_x {
+            let delta_x = mouse_x - previous_x;
+
+            // Si el mouse realmente se movió,
+            // aplicamos la rotación.
+            player.a += delta_x * MOUSE_SENSITIVITY;
+        }
+
+        // Guardamos la posición para el siguiente frame.
+        *last_mouse_x = Some(mouse_x);
     }
 
     if window.is_key_down(Key::S) {
